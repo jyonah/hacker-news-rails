ba2038af208bcb991b7702385105181bb0786e3e
Eric O'Rear & Jonah Price <pair+ejoreo.jyonah@devbootcamp.com>
2017-01-23 15:29:41 -0800

    Add seed file, fix errors in models, tested associations.

--- HackerNews/app/models/comment.rb
+++ HackerNews/app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :commenter, class_name: :user
+  belongs_to :commenter, class_name: :User
   belongs_to :post
 
   validates :body, :post, :commenter, presence: true
--- HackerNews/db/seeds.rb
+++ HackerNews/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+INFO = {
+  "jyonah" => "email@email.com",
+  "eorear" => "otheremail@email.com",
+  "jimmyjohn" => "jj@email.com",
+  "faceman" => "faceemail@email.com",
+  "max" => "maxsemail@email.com",
+}
+
+
+INFO.each do |username, email|
+  User.create(username: username, email: email, password_digest: '1234')
+end
+
+User.all.each do |user|
+  3.times do
+    Post.create(title: Faker::Lorem.sentence, url: Faker::Internet.url, author_id: user.id)
+  end
+end
+
+Post.all.each do |post|
+  rand(3..5).times do
+    Comment.create(post_id: post.id, body: Faker::Lorem.paragraphs(rand(1..4)), commenter_id: User.all.sample.id )
+  end
+end
